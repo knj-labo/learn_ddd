16d7be1633ff4bba6ea5fb78799b4d36a2e0f50f
jp-knj <kenji.tomita1996@gmail.com>
2022-07-21 15:38:32 +0900

    :shower: add nominal type to check unique id

--- backend/src/utils/id.ts
+++
@@ -1,7 +0,0 @@
-import { GeneratedID } from './generated-id'
-import { NoneID } from './none-id'
-
-/**
- * @see https://zenn.dev/zamax/articles/bf33ba94d2b539
- */
-export type ID = GeneratedID | NoneID;
---
+++ backend/src/utils/nominal.ts
@@ -0,0 +1,2 @@
+//@see https://dnlytras.com/blog/nominal-types/
+type Nominal<T, U extends string> = T & { __brand: U }
