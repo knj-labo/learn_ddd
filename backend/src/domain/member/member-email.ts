899ef759ad1136a017c0efaf2d176ac3a9b97409
jp-knj <kenji.tomita1996@gmail.com>
2022-07-21 16:12:51 +0900

    :sparkles: add member email object for creating value object of member

---
+++ backend/src/domain/member/member-email.ts
@@ -0,0 +1,36 @@
+import { ValueObject } from '@utils/value-object'
+
+export interface MemberEmailProps {
+  email: string
+}
+
+/**
+ * @class メンバーのメールアドレスを表す値オブジェクト
+ * @desc MemberEmailオブジェクトのドメインロジックを実装
+ * ユーザーのメールアドレスの検証をカプセル化
+ */
+export class UserEmail extends ValueObject<MemberEmailProps> {
+
+  /**
+   * メンバーのメールアドレスを表す値オブジェクトの新規インスタンスを作成
+   * @param props
+   */
+  private constructor(props: MemberEmailProps) {
+    super(props)
+  }
+
+  /**
+   * @desc メンバーのメールアドレスを取得
+   */
+  get value(): string {
+    return this.props.email
+  }
+
+  /**
+   * @desc
+   * @param email
+   */
+  public static create(email: string) {
+    // Validate email address format
+  }
+}
