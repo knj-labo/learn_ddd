994de8748d149e4e206f247e0eae8d7da522bd97
jp-knj <kenji.tomita1996@gmail.com>
2022-07-25 17:34:09 +0900

    :sparkles: to make the responsibilities easier to understand

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaService } from './database/prisma-service';
-import { MemberService } from './domain/member/member-service';
-import { MemberController } from './presentation/member/member-controller';
+import { MemberController } from './presentation/member/member.controller';
 import { AllMembersQueryService } from './infrastructure/database/all-members-repository';
 
 @Module({
   imports: [],
   controllers: [MemberController],
-  providers: [MemberService, PrismaService, AllMembersQueryService],
+  providers: [PrismaService, AllMembersQueryService],
 })
 export class AppModule {}
