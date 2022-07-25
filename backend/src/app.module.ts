dd3b34d6153dcf2a05402a907a312e2b34ecfa86
jp-knj <kenji.tomita1996@gmail.com>
2022-07-25 18:48:31 +0900

    :sparkles: add usecase to follow onion archtecture

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from './database/prisma-service';
+import { PrismaService } from './database/prisma.service';
 import { MemberController } from './presentation/member/member.controller';
 import { GetAllMembersRepository } from './infrastructure/database/get-all-members.repository';
 import { GetAllMembersUsecase } from "./usecase/member/get-all-members.usecase";
--- backend/src/presentation/member/member.controller.ts
+++ backend/src/presentation/member/member.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from '@nestjs/common';
-// import { GetAllMembersRepository } from '../../infrastructure/database/get-all-members.repository';
 import { GetAllMembersUsecase } from "../../usecase/member/get-all-members.usecase";
 
 @Controller('api/v1/members')
