6b5e21d3a0bff2c9d7645925bc2d173f0b6dd480
jp-knj <kenji.tomita1996@gmail.com>
2022-07-26 22:52:20 +0900

    :sparkles: insert domain logic

--- backend/src/domain/member/member-enrollment-status.ts
+++ backend/src/domain/member/member-enrollment-status.ts
@@ -1,6 +1,40 @@
+import { ValueObject } from '../../utils/value-object';
+
 export interface EnrollmentStatus {
   name: string;
   ENROLLMENT: 'enrolled';
   ABSENCE: 'absented';
   WITHDRAWAL: 'withdraw';
+}
+
+interface EnrollmentStatusProps {
+  name: string;
+}
+/**
+ * @class 参加者の在籍ステータスを表す値オブジェクト
+ * @desc MemberEnrollmentStateオブジェクトのドメインロジックを実装
+ * 参加者の在籍ステータスの検証をカプセル化
+ */
+export class MemberEnrollmentStatus extends ValueObject<EnrollmentStatusProps> {
+  /**
+   * 参加者の在籍ステータスを表す値オブジェクトの新規インスタンスを作成
+   * @param props
+   */
+  public constructor(props: EnrollmentStatusProps) {
+    super(props);
+  }
+
+  public get status(): string {
+    if(!this.isValid(this.props.name)) {
+      throw new Error('ステータスが適切な値ではありません。');
+    }
+    return this.props.name;
+  }
+
+  /**
+   * @desc 'enrolled'か'absented'か'withdraw'かを判定
+   */
+  private isValid(value: string): boolean {
+    return value === 'enrolled' || value === 'absented' || value === 'withdraw';
+  }
 }
