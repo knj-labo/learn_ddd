4bc0ce688e14abf9faece64fc8d8844248525492
jp-knj <kenji.tomita1996@gmail.com>
2022-07-27 14:00:12 +0900

    :sparkles: for creating task aggregate

---
+++ backend/src/domain/task/task-content.test.ts
@@ -0,0 +1,19 @@
+import { TaskContent } from "./task-content";
+import { DomainException } from '../../utils/domain-exception';
+
+describe('課題の内容を受け取った場合で', () => {
+  it('適切な値だったとき、値オブジェクトを作成', () => {
+    const TEST_CONTENT = 'テスト内容';
+
+    const result = TaskContent.create(TEST_CONTENT);
+    expect(result.props.content).toBe(TEST_CONTENT);
+  });
+
+  it('空文字だったとき、例外を投げる', () => {
+    const TEST_CONTENT = ' ';
+    const ERROR_MESSAGE = '内容を入力してください。';
+
+    const result = () => TaskContent.create(TEST_CONTENT);
+    expect(() => result()).toThrow(new DomainException(ERROR_MESSAGE));
+  });
+});
---
+++ backend/src/domain/task/task-content.ts
@@ -0,0 +1,39 @@
+import { ValueObject } from '../../utils/value-object';
+import { DomainException } from '../../utils/domain-exception';
+
+interface TaskContentProps {
+  content: string;
+}
+
+/**
+ * @class 課題のタイトルを表す値オブジェクト
+ */
+export class TaskContent extends ValueObject<TaskContentProps> {
+  /**
+   * @param props
+   */
+  private constructor(props: TaskContentProps) {
+    super(props);
+  }
+
+  private get content(): string {
+    return this.props.content;
+  }
+
+  /**
+   * @desc 空文字ではないかを判定
+   */
+  private static isValid(value): boolean {
+    return value.trim() !== '';
+  }
+
+  /**
+   * @desc ファクトリーメソッド
+   */
+  public static create(content: string): TaskContent{
+    if (!this.isValid(content)) {
+      throw new DomainException('内容を入力してください。');
+    }
+    return new TaskContent({ content: content});
+  }
+}
