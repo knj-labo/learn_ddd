b6b1e6876b0780babbc9b08c8067809eff8f8922
jp-knj <kenji.tomita1996@gmail.com>
2022-07-27 13:49:00 +0900

    :sparkles: insert task title of domain rules

---
+++ backend/src/domain/task/task-title.test.ts
@@ -0,0 +1,19 @@
+import { TaskTitle } from "./task-title";
+import { DomainException } from '../../utils/domain-exception';
+
+describe('課題のタイトルを受け取った場合で', () => {
+  it('適切な値だったとき、値オブジェクトを作成', () => {
+    const TEST_TITLE = 'テスト課題';
+
+    const result = TaskTitle.create(TEST_TITLE);
+    expect(result.title).toBe(TEST_TITLE);
+  });
+
+  it('空文字だったとき、例外を投げる', () => {
+    const TEST_TITLE = ' ';
+    const ERROR_MESSAGE = 'タイトルを入力してください。';
+
+    const result = () => TaskTitle.create(TEST_TITLE);
+    expect(() => result()).toThrow(new DomainException(ERROR_MESSAGE));
+  });
+});
---
+++ backend/src/domain/task/task-title.ts
@@ -0,0 +1,39 @@
+import { ValueObject } from '../../utils/value-object';
+import { DomainException } from '../../utils/domain-exception';
+
+interface TaskTitleProps {
+  title: string;
+}
+
+/**
+ * @class 課題のタイトルを表す値オブジェクト
+ */
+export class TaskTitle extends ValueObject<TaskTitleProps> {
+  /**
+   * @param props
+   */
+  private constructor(props: TaskTitleProps) {
+    super(props);
+  }
+
+  public get title(): string {
+    return this.props.title;
+  }
+
+  /**
+   * @desc 空文字ではないかを判定
+   */
+  private static isValid(value): boolean {
+    return value.trim() !== '';
+  }
+
+  /**
+   * @desc ファクトリーメソッド
+   */
+  public static create(title: string): TaskTitle {
+    if (!this.isValid(title)) {
+      throw new DomainException('タイトルを入力してください。');
+    }
+    return new TaskTitle({ title: title});
+  }
+}
