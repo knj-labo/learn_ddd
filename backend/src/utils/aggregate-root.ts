014020b55140c95eb61bd4418c21cee74ccdcbab
jp-knj <kenji.tomita1996@gmail.com>
2022-07-29 21:27:25 +0900

    :sparkles: 集約のために抽象化クラスを作成

--- backend/src/utils/aggregate-root.ts
+++ backend/src/utils/aggregate-root.ts
@@ -1,6 +1,13 @@
 import { Entity } from '../utils/entity';
 import type { UniqueEntityID } from '../utils/unique-entity-id';
 
+/**
+ * @abstract AggregateRoot<T>
+ * @extends Entity<T> props
+ * @type
+ * T refers to Props
+ * @see https://github.com/4lessandrodev/types-ddd/blob/eded1cf4c7f966b6ba916a95be7569b7e25a951f/lib/core/entity.ts#L574
+ */
 export abstract class AggregateRoot<T> extends Entity<T> {
   get id(): UniqueEntityID {
     return this._id;
