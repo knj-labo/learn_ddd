e06653064126597a35c6b7f5e30123bbdcbe71cf
jp-knj <kenji.tomita1996@gmail.com>
2022-07-29 23:39:34 +0900

    :sparkles: create seeder of aggregate

---
+++ backend/src/database/seed/task-assignee.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export const taskAssignee = async () => {
+  await prisma.taskAssignee.createMany({
+    data: Array(80)
+      .fill(0)
+      .map((_, index) => ({
+        id: index + 1,
+        taskId: index + 1,
+        memberId: 1,
+        taskProgressStatusId: 1,
+      }))
+  });
+};
